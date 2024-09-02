2a84a7407c75b4b1e8f6c690664ed44811e62f5e
FOERSTAT21 <FoerstAt21@gcc.edui>
2024-09-02 16:33:28 -0400

    Added Comments

--- cracker.py
+++ cracker.py
@@ -1,10 +1,18 @@
-from datetime import datetime 
+"""
+File name: cracker.py
+Author: Andrew Foerst 
+Created: 9/1/2024
+Description: To display all possible combinations of a word lock 
+"""
+
 def main():
+    # Loading dictionary of words 
     words = set()
     with open("words.txt", "rt", encoding="utf-8") as read:
         for line in read:
             words.add(line.strip())
 
+    # Getting user input for what the combinations can be
     first = list(input("What are the options for the first letter of the lock?\n").upper())
     second = list(input("What are the options for the second letter of the lock?\n").upper())
     third = list(input("What are the options for the third letter of the lock?\n").upper())
@@ -13,6 +21,7 @@ def main():
     test = ["0","0","0","0"]
     possibilities = []
 
+    # Generating all possibilities 
     for f in first:
         test[0] = f
         for s in second:
@@ -24,6 +33,8 @@ def main():
                     word = "".join(test)
                     if word in words:
                         possibilities.append(word)
+    
+    # Outputting possible words for the lock into a text file 
     possibilities.sort()
     with open("possibilities.txt", "wt", encoding="utf-8") as w:
         for p in possibilities:
